3e00d475a468fe0b44e8a02b4aa4c930ae4991da
Caleb George <caleb.george@cythereal.com>
2023-10-11 16:39:25 -0500

    Added hexToDecimal function to help with pointer-related conversions

--- UnknownCyberPlugin/src/main/java/unknowncyberplugin/helpers.java
+++ UnknownCyberPlugin/src/main/java/unknowncyberplugin/helpers.java
@@ -6,15 +6,18 @@ import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.xml.bind.DatatypeConverter;
 
 public class helpers {
-
-  // Note:  There are a handful of use cases where this will fail:
-  // - The original file is no longer on the host's system
-  //    - This might also fail if the original file has been moved from where
-  //      it was first imported from; I have not tested for this edge case
-  // - The file was imported from an archive through the "Batch" option
+  /**
+   * Note:  There are a handful of use cases where this will fail:
+   * - The original file is no longer on the host's system
+   *   - This might also fail if the original file has been moved from where
+   *     it was first imported from; I have not tested for this edge case
+   * - The file was imported from an archive through the "Batch" option
+   */
   public static String hashFile(File file, String algo) throws IOException, NoSuchAlgorithmException {
     MessageDigest digest = MessageDigest.getInstance(algo);
     FileInputStream fis = new FileInputStream(file);
@@ -30,16 +33,18 @@ public class helpers {
     return DatatypeConverter.printHexBinary(digest.digest());
   }
 
-  // There appear to be a handful of cases where an address is not
-  //   formatted as either a hex value.  Currently known exceptions include:
-  //   - ram:012abc
-  // This function iterates from the end of a string address, accepting all
-  //   hex-acceptable values.  If the start of the string is reached, the
-  //   original string is returned.  If a non-hex value is reached, the hex
-  //   string from that point forward (without the illegal character) will
-  //   be returned.
-  // It also has an escape hatch when it detects that a string is either wholly
-  //   hex characters, or matches 0x... hex format
+  /** 
+   * There appear to be a handful of cases where an address is not
+   *   formatted as either a hex value.  Currently known exceptions include:
+   *   - ram:012abc
+   * This function iterates from the end of a string address, accepting all
+   *   hex-acceptable values.  If the start of the string is reached, the
+   *   original string is returned.  If a non-hex value is reached, the hex
+   *   string from that point forward (without the illegal character) will
+   *   be returned.
+   * It also has an escape hatch when it detects that a string is either wholly
+   *   hex characters, or matches 0x... hex format
+   */
   public static String cleanAddress(String address) {
     address = address.toLowerCase();
 
@@ -74,5 +79,27 @@ public class helpers {
 
     // Fallback return, necessary for compilation but should never be needed
     return address;
+  }
+
+  /**
+   * Recursively searches a String value for hexadecimal matches and converts them to decimal values
+   *   - Expected hex format: 0x...
+   */
+  public static String hexToDecimal(String input) {
+    Matcher matcher = Pattern.compile("0x[0-9a-fA-F]+").matcher(input);
+    if (matcher.find()) {
+      // Extract the first hex value, removing the 0x prefix
+      String hex = matcher.group().substring(2);
+
+      String decimal = Integer.toString(Integer.parseInt(hex, 16));
+
+      String result = matcher.replaceFirst(decimal);
+
+      // Recurse for next potential match
+      return hexToDecimal(result);
+    }
+
+    // If no match is found, return original input
+    return input;
   }
 }
